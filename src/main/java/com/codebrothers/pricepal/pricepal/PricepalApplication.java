bff49884b2ec1ee4cd3c504fb2fb9d962b2d7685
skifiltos <g.c.faina@gmail.com>
2024-08-24 18:44:52 +0200

    first commit

---
+++ src/main/java/com/codebrothers/pricepal/pricepal/PricepalApplication.java
@@ -0,0 +1,13 @@
+package com.codebrothers.pricepal.pricepal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricepalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricepalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebrothers/pricepal/pricepal/PricepalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebrothers.pricepal.pricepal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricepalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
